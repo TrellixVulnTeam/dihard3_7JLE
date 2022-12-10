6da24ad763623fb41f46cbf15c2316b3f7d24686
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 00:52:44 +0000

    Adding tarfile member sanitization to extractall()

--- docs/validate_submission.py
+++ docs/validate_submission.py
@@ -103,7 +103,29 @@ def main():
             check_submission(Path(tmp_dir, args.submission.stem))
         elif tarfile.is_tarfile(args.submission):
             with tarfile.open(args.submission, 'r:*') as f:
-                f.extractall(tmp_dir)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, tmp_dir)
             check_submission(Path(tmp_dir, args.submission.stem))
         else:
             error(f'Unknown archive format: "{args.submission}".')
